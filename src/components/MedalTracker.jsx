5109fa40d904f5aa92f9c644db16c9739115c045
whgmlwls0219 <whgmlwls0219@naver.com>
2024-10-15 20:39:23 +0900

    crud 모든 기능 완성/ css 제외

--- src/components/MedalTracker.jsx
+++ src/components/MedalTracker.jsx
@@ -40,10 +40,39 @@ const MedalTracker = () => {
   };
 
   // 삭제 버튼 click 함수
+  // 해당 국가를 제외한 배열을 만듦
   const handleDeleteNation = (name) => {
     const filteredData = userData.filter((item) => item.nation !== name);
-    // console.log(filteredData);
+    // nation 값이 일치하지 않는 항목들만 남긴 새로운 배열을 만듦
     setUserData(filteredData);
+  };
+  // name: 삭제하려는 특정 국가명
+  // userData: 여러 국가와 + 국가의 메달 정보 (→ 행)
+  // filter(): 조건에 맞는 항목들만 배열로 반환
+  // 클릭한 국가 + 동일한 국가만 배열에서 삭제
+  // 나머지는 그대로 남아있음
+
+  // 수정 버튼 click 함수
+  // nation 값이 현재 입력된 국가명과 일치하는 항목을 찾음
+  const handleUpdateNation = () => {
+    // nation 값이 일치하면 입력된 값으로 해당 항목을 업데이트
+    const updatedData = userData.find((item) => item.nation === nation);
+    // 사용자가 입력한 국가명과 일치하는 항목이 있다면
+    if (updatedData) {
+      // 이전 상태의 userData 배열을 기반으로 업데이트
+      setUserData((prev) => {
+        // 이전 상태의 userData 배열을 복사한 후 각 항목을 순회하며 검사
+        let result = [...prev].map((item) => {
+          // 일치하면 사용자가 입력한 값을 반영한 새 객체로 교체
+          // 일치하지 않으면 기존 값을 그대로 유지
+          return item.nation === updatedData.nation
+            ? { nation, goldMedal, silverMedal, bronzeMedal }
+            : item;
+        });
+        // 업데이트된 배열을 반환하여 userData 상태를 업데이트
+        return result;
+      });
+    }
   };
 
   return (
@@ -82,7 +111,7 @@ const MedalTracker = () => {
               onChange={(e) => BronzeMedalHandleInputChange(e)}
             ></input>
             <button onClick={(e) => handleClick(e)}>추가</button>
-            <button>수정</button>
+            <button onClick={(e) => handleUpdateNation()}>수정</button>
           </div>
         </div>
         {/* 값*/}
